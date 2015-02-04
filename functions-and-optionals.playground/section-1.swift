bcc59f294300620a4dbfcba83e45d3ad797f7b4c
Roy McFarland <roy.mcfarland@gmail.com>
2015-02-03 18:11:24 -0700

    Initial commit

---
+++ functions-and-optionals.playground/section-1.swift
@@ -0,0 +1,16 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+////////////////////////////////////////
+//////// CREATING A FUNCTION ///////////
+////////////////////////////////////////
+
+func calculateArea() {
+    let height = 12
+    let width = 10
+    let area = height * width
+    println("The area of the room is \(area)")
+}
+
+calculateArea()
